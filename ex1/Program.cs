4092328d7920a43f3e09cca43742b3fff87144dc
MANUELA <MANUELA@DESKTOP-T34Q0BV>
2023-03-25 22:34:17 -0300

    Adicionar arquivos de projeto.

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            Console.WriteLine("Insira um valor que esteja entre a faixa de 1 a 9: ");
+            numero = Convert.ToSingle(Console.ReadLine());
+
+            if ((numero < 1) || (numero > 9))
+            {
+                Console.WriteLine("O valor está fora da faixa permitida.");
+
+            }
+            else
+            {
+                Console.WriteLine("O valor está na faixa permitida.");
+            }
+
+        }
+    }
+}
